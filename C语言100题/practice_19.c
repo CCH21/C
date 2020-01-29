bb23808b53d20495042078d31664bf111ed37db1
CCH21 <1398635912@qq.com>
2020-01-29 22:39:17 +0800

    C语言练习

---
+++ C语言100题/practice_19.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	unsigned int n;
+	scanf("%d", &n);
+	int num, t;
+	for (int i = 31; i >= 0; i--)
+	{
+		num = n;
+		num = num >> (31 - i);
+		t = num & 1;
+		printf("%d", t);
+	}
+	return 0;
+}
